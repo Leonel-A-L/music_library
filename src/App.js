edf4ec7c733170dc93d78dd33501f08f5f8b3830
Leonel-A-L <lamelas.leonel@gmail.com>
2023-06-04 10:33:58 -0400

    added context to the library

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery.js'
 import SearchBar from './components/SearchBar'
+import { DataContext } from './Contexts/DataContext'
 
 function App(){
     let [search, setSearch] = useState('')
@@ -33,7 +34,9 @@ function App(){
         <div>
             <SearchBar handleSearch={handleSearch}/>
             {message}
-            <Gallery data={data}/>
+            <DataContext.Provider value={data}>
+              <Gallery />
+            </DataContext.Provider>            
         </div>
     )
 }

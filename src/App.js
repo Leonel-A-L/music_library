885d87184cb8b728c3b7c9a1dead28d5546a8cba
Leonel-A-L <lamelas.leonel@gmail.com>
2023-06-06 18:35:23 -0400

    added the routes for artists and songs

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
-import './App.css'
 import { useEffect, useState } from 'react'
-import Gallery from './components/Gallery.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
-function App(){
-    let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const API_URL = `https://itunes.apple.com/search?term=${encodeURI(search)}` 
-        const response = await fetch (API_URL)
-        const data = await response.json()
-        console.log(data)
-        if(data.results.length > 0){
-          setData(data.results)
-        }else{
-          setMessage('Not Found')
-        }
-      }
-      if (search) fetchData()
-    }, [search])
+function App() {
+	let [search, setSearch] = useState('')
+	let [message, setMessage] = useState('Search for Music!')
+	let [data, setData] = useState([])
 
-    const handleSearch = (e, term) => {
-      e.preventDefault()
-      setSearch(term)
-    }
-    
+	const API_URL = 'https://itunes.apple.com/search?term='
 
-    return (
-        <div>
-            <SearchBar handleSearch={handleSearch}/>
-            {message}
-            <Gallery data={data}/>
-        </div>
-    )
+	useEffect(() => {
+		if(search) {
+			const fetchData = async () => {
+				document.title = `${search} Music`
+				const response = await fetch(API_URL + search)
+				const resData = await response.json()
+				if (resData.results.length > 0) {
+					return setData(resData.results)
+				} else {
+					return setMessage('Not Found')
+				}
+			}
+			fetchData()
+		}
+	}, [search])
+	
+	const handleSearch = (e, term) => {
+		e.preventDefault()
+		setSearch(term)
+	}
+
+	return (
+		<div>
+			{message}
+			<Router>
+				<Routes>
+					<Route path="/" element={
+						<>
+							<SearchBar handleSearch = {handleSearch}/>
+							<Gallery data={data} />
+						</>
+					} />
+					<Route path="/album/:id" element={<AlbumView />} />
+					<Route path="/artist/:id" element={<ArtistView />} />
+				</Routes>
+			</Router>
+		</div>
+  	);
 }
 
-export default App
+export default App;
